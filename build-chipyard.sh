db73b59f5d5452957b9fee3d7eec0b361891c366
Matthew Mikhailov <mlmikhai@ucsd.edu>
2023-04-21 02:40:30 +0000

    make the build chipyard script install in people's home directory

--- build-chipyard.sh
+++ build-chipyard.sh
@@ -1,5 +1,6 @@
-# sudo chmod a+r /boot/vmlinuz-5.15.0-69-generic
+#sudo chmod a+r /boot/vmlinuz-5.15.0-69-generic
 # sudo chmod a+r /boot/vmlinuz-5.15.0-70-generic
+cd ~
 wget https://github.com/conda-forge/miniforge/releases/download/22.11.1-4/Mambaforge-22.11.1-4-Linux-x86_64.sh
 chmod u+x Mambaforge-22.11.1-4-Linux-x86_64.sh
 ./Mambaforge-22.11.1-4-Linux-x86_64.sh
